c5b3a65be9070f2700a415e6f5f3c2cffce64b19
Ravindra Tummuru <Ravindra.Tummuru@ext.ons.gov.uk>
2026-08-16 18:12:48 +0100

    CN-124 Clean up of unnecessary logging and their tests and also cleaned up pubsub config not required by the service tests.

--- src/main/java/uk/gov/ons/census/fieldworkadapter/logging/EventLogger.java
+++ src/main/java/uk/gov/ons/census/fieldworkadapter/logging/EventLogger.java
@@ -6,10 +6,8 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
-import uk.gov.ons.census.common.model.entity.Case;
 import uk.gov.ons.census.common.model.entity.Event;
 import uk.gov.ons.census.common.model.entity.EventType;
-import uk.gov.ons.census.common.model.entity.UacQidLink;
 import uk.gov.ons.census.fieldworkadapter.model.dto.EventDTO;
 import uk.gov.ons.census.fieldworkadapter.model.dto.EventHeaderDTO;
 import uk.gov.ons.census.fieldworkadapter.model.repository.EventRepository;
@@ -23,67 +21,6 @@ public class EventLogger {
 
   public EventLogger(EventRepository eventRepository) {
     this.eventRepository = eventRepository;
-  }
-
-  public void logCaseEvent(
-      Case caze,
-      String eventDescription,
-      EventType eventType,
-      EventDTO event,
-      OffsetDateTime messageTimestamp) {
-
-    EventHeaderDTO eventHeader = event.getHeader();
-    OffsetDateTime eventDate = eventHeader.getDateTime();
-    Object eventPayload = event.getPayload();
-
-    Event loggedEvent =
-        buildEvent(
-            eventDate,
-            eventDescription,
-            eventType,
-            eventHeader,
-            RedactHelper.redact(eventPayload),
-            messageTimestamp);
-    loggedEvent.setCaze(caze);
-
-    eventRepository.save(loggedEvent);
-  }
-
-  public void logCaseEvent(
-      Case caze,
-      String eventDescription,
-      EventType eventType,
-      EventDTO event,
-      Message<byte[]> message) {
-
-    OffsetDateTime messageTimestamp = getMessageTimeStamp(message);
-
-    logCaseEvent(caze, eventDescription, eventType, event, messageTimestamp);
-  }
-
-  public void logUacQidEvent(
-      UacQidLink uacQidLink,
-      String eventDescription,
-      EventType eventType,
-      EventDTO event,
-      Message<byte[]> message) {
-
-    EventHeaderDTO eventHeader = event.getHeader();
-    OffsetDateTime eventDate = eventHeader.getDateTime();
-    Object eventPayload = event.getPayload();
-    OffsetDateTime messageTimestamp = getMessageTimeStamp(message);
-
-    Event loggedEvent =
-        buildEvent(
-            eventDate,
-            eventDescription,
-            eventType,
-            eventHeader,
-            RedactHelper.redact(eventPayload),
-            messageTimestamp);
-    loggedEvent.setUacQidLink(uacQidLink);
-
-    eventRepository.save(loggedEvent);
   }
 
   public void logEvent(
--- src/test/java/uk/gov/ons/census/fieldworkadapter/logging/EventLoggerTest.java
+++ src/test/java/uk/gov/ons/census/fieldworkadapter/logging/EventLoggerTest.java
@@ -16,16 +16,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
-import uk.gov.ons.census.common.model.entity.Case;
 import uk.gov.ons.census.common.model.entity.Event;
 import uk.gov.ons.census.common.model.entity.EventType;
-import uk.gov.ons.census.common.model.entity.UacQidLink;
 import uk.gov.ons.census.fieldworkadapter.model.dto.EventDTO;
 import uk.gov.ons.census.fieldworkadapter.model.dto.EventHeaderDTO;
 import uk.gov.ons.census.fieldworkadapter.model.dto.FieldActionInstruction;
 import uk.gov.ons.census.fieldworkadapter.model.dto.FwmtActionInstructionDTO;
-import uk.gov.ons.census.fieldworkadapter.model.dto.NewCase;
-import uk.gov.ons.census.fieldworkadapter.model.dto.PayloadDTO;
 import uk.gov.ons.census.fieldworkadapter.model.repository.EventRepository;
 import uk.gov.ons.census.fieldworkadapter.utils.EventHelper;
 
@@ -34,137 +30,6 @@ class EventLoggerTest {
   @Mock EventRepository eventRepository;
 
   @InjectMocks EventLogger underTest;
-
-  @Test
-  void testLogCaseEventSuppliedDateTime() {
-    Case caze = new Case();
-    OffsetDateTime eventTime = OffsetDateTime.now();
-    OffsetDateTime messageTime = OffsetDateTime.now().minusSeconds(30);
-    EventHeaderDTO eventHeader =
-        EventHelper.createEventDTO("Test topic", TEST_CORRELATION_ID, TEST_ORIGINATING_USER);
-    eventHeader.setSource("Test source");
-    eventHeader.setChannel("Test channel");
-    eventHeader.setDateTime(eventTime);
-    EventDTO event = new EventDTO();
-    event.setHeader(eventHeader);
-
-    NewCase redactMe = new NewCase();
-
-    PayloadDTO payload = new PayloadDTO();
-    payload.setNewCase(redactMe);
-    event.setPayload(payload);
-
-    underTest.logCaseEvent(caze, "Test description", EventType.NEW_CASE, event, messageTime);
-
-    ArgumentCaptor<Event> eventArgumentCaptor = ArgumentCaptor.forClass(Event.class);
-    verify(eventRepository).save(eventArgumentCaptor.capture());
-    Event actualEvent = eventArgumentCaptor.getValue();
-    assertThat(caze).isEqualTo(actualEvent.getCaze());
-    assertThat(actualEvent.getUacQidLink()).isNull();
-    assertThat(eventTime).isEqualTo(actualEvent.getDateTime());
-    assertThat("Test source").isEqualTo(actualEvent.getSource());
-    assertThat("Test channel").isEqualTo(actualEvent.getChannel());
-    assertThat(EventType.NEW_CASE).isEqualTo(actualEvent.getType());
-    assertThat("Test description").isEqualTo(actualEvent.getDescription());
-    assertThat(eventHeader.getMessageId()).isEqualTo(actualEvent.getMessageId());
-    assertThat(eventHeader.getCorrelationId()).isEqualTo(actualEvent.getCorrelationId());
-    assertThat(eventHeader.getOriginatingUser()).isEqualTo(actualEvent.getCreatedBy());
-    assertThat(messageTime).isEqualTo(actualEvent.getMessageTimestamp());
-  }
-
-  @Test
-  void testLogCaseEventDateTimeFromMessage() {
-    Case caze = new Case();
-    OffsetDateTime eventTime = OffsetDateTime.now();
-    EventHeaderDTO eventHeader =
-        EventHelper.createEventDTO("Test topic", TEST_CORRELATION_ID, TEST_ORIGINATING_USER);
-    eventHeader.setSource("Test source");
-    eventHeader.setChannel("Test channel");
-    eventHeader.setDateTime(eventTime);
-    EventDTO event = new EventDTO();
-    event.setHeader(eventHeader);
-
-    NewCase redactMe = new NewCase();
-
-    PayloadDTO payload = new PayloadDTO();
-    payload.setNewCase(redactMe);
-    event.setPayload(payload);
-
-    Message<byte[]> message = mock(Message.class);
-
-    // Now the timestamp fun.  Get one an hour+ ago, to 'prove' no fluking
-    OffsetDateTime messageTime = OffsetDateTime.now().minusSeconds(3911);
-    long timeStamp = messageTime.toInstant().toEpochMilli();
-
-    MessageHeaders messageHeaders = mock(MessageHeaders.class);
-    when(message.getHeaders()).thenReturn(messageHeaders);
-
-    when(messageHeaders.getTimestamp()).thenReturn(timeStamp);
-
-    underTest.logCaseEvent(caze, "Test description", EventType.NEW_CASE, event, message);
-
-    ArgumentCaptor<Event> eventArgumentCaptor = ArgumentCaptor.forClass(Event.class);
-    verify(eventRepository).save(eventArgumentCaptor.capture());
-    Event actualEvent = eventArgumentCaptor.getValue();
-    assertThat(caze).isEqualTo(actualEvent.getCaze());
-    assertThat(actualEvent.getUacQidLink()).isNull();
-    assertThat(eventTime).isEqualTo(actualEvent.getDateTime());
-    assertThat("Test source").isEqualTo(actualEvent.getSource());
-    assertThat("Test channel").isEqualTo(actualEvent.getChannel());
-    assertThat(EventType.NEW_CASE).isEqualTo(actualEvent.getType());
-    assertThat("Test description").isEqualTo(actualEvent.getDescription());
-    assertThat(eventHeader.getMessageId()).isEqualTo(actualEvent.getMessageId());
-    assertThat(eventHeader.getCorrelationId()).isEqualTo(actualEvent.getCorrelationId());
-    assertThat(eventHeader.getOriginatingUser()).isEqualTo(actualEvent.getCreatedBy());
-    assertThat(timeStamp).isEqualTo(actualEvent.getMessageTimestamp().toInstant().toEpochMilli());
-  }
-
-  @Test
-  void testLogUacQidEvent() {
-    UacQidLink uacQidLink = new UacQidLink();
-    OffsetDateTime eventTime = OffsetDateTime.now();
-    EventHeaderDTO eventHeader =
-        EventHelper.createEventDTO("Test topic", TEST_CORRELATION_ID, TEST_ORIGINATING_USER);
-    eventHeader.setSource("Test source");
-    eventHeader.setChannel("Test channel");
-    eventHeader.setDateTime(eventTime);
-    EventDTO event = new EventDTO();
-    event.setHeader(eventHeader);
-
-    NewCase redactMe = new NewCase();
-
-    PayloadDTO payload = new PayloadDTO();
-    payload.setNewCase(redactMe);
-    event.setPayload(payload);
-
-    Message<byte[]> message = mock(Message.class);
-
-    // Now the timestamp fun.  Get one an hour+ ago, to 'prove' no fluking
-    OffsetDateTime messageTime = OffsetDateTime.now().minusSeconds(3911);
-    long timeStamp = messageTime.toInstant().toEpochMilli();
-
-    MessageHeaders messageHeaders = mock(MessageHeaders.class);
-    when(message.getHeaders()).thenReturn(messageHeaders);
-
-    when(messageHeaders.getTimestamp()).thenReturn(timeStamp);
-
-    underTest.logUacQidEvent(uacQidLink, "Test description", EventType.NEW_CASE, event, message);
-
-    ArgumentCaptor<Event> eventArgumentCaptor = ArgumentCaptor.forClass(Event.class);
-    verify(eventRepository).save(eventArgumentCaptor.capture());
-    Event actualEvent = eventArgumentCaptor.getValue();
-    assertThat(uacQidLink).isEqualTo(actualEvent.getUacQidLink());
-    assertThat(actualEvent.getCaze()).isNull();
-    assertThat(eventTime).isEqualTo(actualEvent.getDateTime());
-    assertThat("Test source").isEqualTo(actualEvent.getSource());
-    assertThat("Test channel").isEqualTo(actualEvent.getChannel());
-    assertThat(EventType.NEW_CASE).isEqualTo(actualEvent.getType());
-    assertThat("Test description").isEqualTo(actualEvent.getDescription());
-    assertThat(eventHeader.getMessageId()).isEqualTo(actualEvent.getMessageId());
-    assertThat(eventHeader.getCorrelationId()).isEqualTo(actualEvent.getCorrelationId());
-    assertThat(eventHeader.getOriginatingUser()).isEqualTo(actualEvent.getCreatedBy());
-    assertThat(timeStamp).isEqualTo(actualEvent.getMessageTimestamp().toInstant().toEpochMilli());
-  }
 
   @Test
   void testLogEventDateTimeFromMessage() {
